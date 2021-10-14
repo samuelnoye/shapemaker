439e5c2f32347a46b34d17bf2a12445f091b0530
Noye Samuel <82453244+samuelnoye@users.noreply.github.com>
2021-10-14 07:00:51 -0700

    Square

--- Shapes.kt
+++ Shapes.kt
@@ -1,5 +1,19 @@
 fun main() {
-
+ // Square Variables
+  val sqSide = 7
+  val sqChar1 = "X  "
+  val sqChar2 = "O  "
+  // Write your code below
+  for( i in 1..sqSide){
+    for( j in 1..sqSide){
+    if((i % 2 == 0 && j % 2 == 0)||(i % 2 == 1 && j % 2 == 1)){
+      print(sqChar1)
+    }else{
+      print(sqChar2)
+    }
+  }
+   println()
+  }
 
 
 }
